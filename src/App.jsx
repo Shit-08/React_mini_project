12e9e9bf8466949f3963bb7f32fe46540521c62f
Shital Patil <patilshitalbharat@gmail.com>
2025-10-28 20:19:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1> Material UI</h1>
+      <Button>Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  );
+}
+
+export default App;
