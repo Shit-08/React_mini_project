649399402bfde9ad60f3fb77cd195fea45019502
Shital Patil <patilshitalbharat@gmail.com>
2025-10-28 20:53:18 +0530

    added form for searching city

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Button from "@mui/material/Button";
+import SearchBox from "./SearchBox";
 
 function App() {
   return (
     <>
-      <h1> Material UI</h1>
-      <Button>Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <SearchBox />
     </>
   );
 }
---
+++ src/SearchBox.jsx
@@ -0,0 +1,32 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+
+export default function SearchBox() {
+  let [city, setCity] = useState("");
+
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  return (
+    <div className="SearchBox">
+      <h3>Search for the Weather!</h3>
+      <form>
+        <TextField
+          required
+          id="city"
+          label="City Name"
+          variant="outlined"
+          value={city}
+          onChange={handleChange}
+        />
+        <br></br> <br></br>
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+}
